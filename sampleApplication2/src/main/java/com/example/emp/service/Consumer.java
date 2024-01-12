6f592348a06088c46046c5699aeb34e93bf61be1
alokdhirhcl <75309216+alokdhirhcl@users.noreply.github.com>
2024-01-12 10:05:48 +0530

    Initial commit

---
+++ sampleApplication2/src/main/java/com/example/emp/service/Consumer.java
@@ -0,0 +1,18 @@
+package com.example.emp.service;
+
+
+
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+
+@Service
+public class Consumer {
+	
+	
+
+}
